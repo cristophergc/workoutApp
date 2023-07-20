3a1c38f2fa32cacb8cbcd990d40cd306604537af
Cristopher-Germuts <cristophergc@hotmail.com>
2023-07-20 18:52:11 +1200

    first commit

---
+++ app/src/main/java/com/example/a7minuteworkout/Constants.kt
@@ -0,0 +1,116 @@
+package com.example.a7minuteworkout
+
+object Constants {
+    fun defaultExerciseList(): ArrayList<ExerciseModel>{
+        val exerciseList = ArrayList<ExerciseModel>()
+        val jumpingJacks = ExerciseModel(
+            1,
+            "Jumping Jacks",
+            R.drawable.ic_jumping_jacks,
+            false,
+            false
+        )
+        exerciseList.add(jumpingJacks)
+
+        val abdominalCrunch = ExerciseModel(
+            2,
+            "Abdominal Crunches",
+            R.drawable.ic_abdominal_crunch,
+            false,
+            false
+        )
+        exerciseList.add(abdominalCrunch)
+
+        val highKneesRunning = ExerciseModel(
+            3,
+            "High Knees Running",
+            R.drawable.ic_high_knees_running_in_place,
+            false,
+            false
+        )
+        exerciseList.add(highKneesRunning)
+
+        val lunge = ExerciseModel(
+            4,
+            "Lunges",
+            R.drawable.ic_lunge,
+            false,
+            false
+        )
+        exerciseList.add(lunge)
+
+        val plank = ExerciseModel(
+            5,
+            "Plank",
+            R.drawable.ic_plank,
+            false,
+            false
+        )
+        exerciseList.add(plank)
+
+        val pushUp = ExerciseModel(
+            6,
+            "Push Up",
+            R.drawable.ic_push_up,
+            false,
+            false
+        )
+        exerciseList.add(pushUp)
+
+        val pushUpAndRotation = ExerciseModel(
+            7,
+            "Push up and rotation",
+            R.drawable.ic_push_up_and_rotation,
+            false,
+            false
+        )
+        exerciseList.add(pushUpAndRotation)
+
+        val sidePlank = ExerciseModel(
+            8,
+            "Side Plank",
+            R.drawable.ic_side_plank,
+            false,
+            false
+        )
+        exerciseList.add(sidePlank)
+
+        val squat = ExerciseModel(
+            9,
+            "Squat",
+            R.drawable.ic_squat,
+            false,
+            false
+        )
+        exerciseList.add(squat)
+
+        val stepUpOntoChair = ExerciseModel(
+            10,
+            "Step up onto chair",
+            R.drawable.ic_step_up_onto_chair,
+            false,
+            false
+        )
+        exerciseList.add(stepUpOntoChair)
+
+        val tricepsDipOnChair = ExerciseModel(
+            11,
+            "Triceps dip on chair",
+            R.drawable.ic_triceps_dip_on_chair,
+            false,
+            false
+        )
+        exerciseList.add(tricepsDipOnChair)
+
+        val wallSit = ExerciseModel(
+            12,
+            "Wall sit",
+            R.drawable.ic_wall_sit,
+            false,
+            false
+        )
+        exerciseList.add(wallSit)
+
+        return exerciseList
+    }
+}
